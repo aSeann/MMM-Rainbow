5c239faa2cb15a93aa9c11e0be9369335787c03c
aSeann <31374101+aSeann@users.noreply.github.com>
2019-01-23 23:56:18 +0000

    Update MMM-Rainbow.js

--- MMM-Rainbow.js
+++ MMM-Rainbow.js
@@ -1,21 +1,39 @@
 Module.register("MMM-Rainbow",{
 	defaults: {
 		fadeTime: 1000,
-		nextColor: 1000
+		nextColor: 1000,
+		modular: true,
+		moduleList: [
+			"MMM-iClock",
+			"MMM-Showtimes"
+		]
 	},
 	getScripts: function() {
 		return ["jquery.js"];
 	},
 	start: function() {
-		Log.info("Starting module: " + this.name);
-		var style = `<style>
-			.dimmed, .normal, .bright, .large .wi, .module-header, .day, .min-temp, .clock{
-				transition:`+this.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
-				-o-transition:`+this.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
-				-ms-transition:`+this.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
-				-moz-transition:`+this.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
-				-webkit-transition:`+this.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
+		self = this;
+		Log.info("Starting module: " + self.name);
+		var classes = [".dimmed", ".normal", ".bright", ".wi", ".module-header", ".day", ".min-temp"];		//	, ".xsmall", ".small", ".medium", ".large", ".xlarge"
+		var style = "";
+		if(self.config.modular){
+			item = [];
+			for(var i = 0; i < self.config.moduleList.length; i++){
+				for(var j = 0; j < classes.length; j++){
+					style += "." + self.config.moduleList[i] + " " + classes[j] + ", ";
+				}
+				item.push("." + self.config.moduleList[i]);
 			}
+			style = style.substr(0, style.length - 2);
+		} else style = "html, .dimmed, .normal, .bright, .medium, .large .wi, .module-header, .day, .min-temp, .clock";
+		style = `<style>
+		` + style + `{
+			transition:`+self.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
+			-o-transition:`+self.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
+			-ms-transition:`+self.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
+			-moz-transition:`+self.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
+			-webkit-transition:`+self.config.fadeTime+`ms cubic-bezier(.4, 0, .2, 1);
+		}
 		</style>`;
 		$('head').append(style);
 		var R, G, B;
@@ -23,9 +41,18 @@ Module.register("MMM-Rainbow",{
 			R = Math.floor(Math.random() * 256);
 			G = Math.floor(Math.random() * 256);
 			B = Math.floor(Math.random() * 256);
-			$('.dimmed').css("color", "rgba("+R+", "+G+", "+B+", .4)");
-			$('.normal').css("color", "rgba("+R+", "+G+", "+B+", .7)");
-			$('body, header, .bright').css("color", "rgba("+R+", "+G+", "+B+", 1)");
-		}, this.config.nextColor);
+			if(self.config.modular){
+				for(var x = 0; x < item.length; x++){
+					$(item[x] + " .dimmed").css("color", "rgba("+R+", "+G+", "+B+", .4)");
+					$(item[x] + " .normal").css("color", "rgba("+R+", "+G+", "+B+", .7)");
+					$(item[x] + " .bright").css("color", "rgba("+R+", "+G+", "+B+", 1)");
+				}
+			} else {
+				$('html, header').css("border-color", "rgba("+R+", "+G+", "+B+", .4)");
+				$('.dimmed').css("color", "rgba("+R+", "+G+", "+B+", .4)");
+				$('.normal').css("color", "rgba("+R+", "+G+", "+B+", .7)");
+				$('body, header, .bright').css("color", "rgba("+R+", "+G+", "+B+", 1)");
+			}
+		}, self.config.nextColor);
 	}
 });
